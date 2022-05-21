b253e0622d665e159b6af9f6ee2eccb36a303cbc
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-05-20 20:45:40 -0600

    perf(libre.system): add voting

--- contracts/libre.system/src/libre.system.cpp
+++ contracts/libre.system/src/libre.system.cpp
@@ -10,10 +10,25 @@ namespace libresystem {
 
    system_contract::system_contract( name s, name code, datastream<const char*> ds )
    :native(s,code,ds),
-    _producers(get_self(), get_self().value)
-   {}
+    _voters(get_self(), get_self().value),
+    _producers(get_self(), get_self().value),
+    _global(get_self(), get_self().value),
+    _global2(get_self(), get_self().value)
+   {
+      _gstate  = _global.exists() ? _global.get() : get_default_parameters();
+      _gstate2 = _global2.exists() ? _global2.get() : eosio_global_state2{};
+   }
 
-   system_contract::~system_contract() {}
+   eosio_global_state system_contract::get_default_parameters() {
+      eosio_global_state dp;
+      get_blockchain_parameters(dp);
+      return dp;
+   }
+
+   system_contract::~system_contract() {
+      _global.set( _gstate, get_self() );
+      _global2.set( _gstate2, get_self() );
+   }
 
    void system_contract::setparams( const eosio::blockchain_parameters& params ) {
       require_auth( get_self() );
--- contracts/libre.system/src/voting.cpp
+++ contracts/libre.system/src/voting.cpp
@@ -61,11 +61,23 @@ namespace libresystem {
    void system_contract::regproducer( const name& producer, const eosio::public_key& producer_key, const std::string& url, uint16_t location ) {
       require_auth( producer );
 
+      check( url.size() < 512, "url too long" );
       check (checkPermission(producer, "regprod") == 1, "You are not authorised to register as producer");  // LIBRE Check Permissions
 
-      check( url.size() < 512, "url too long" );
-
       register_producer( producer, convert_to_block_signing_authority( producer_key ), url, location );
+   }
+
+   void system_contract::regproducer2( const name& producer, const eosio::block_signing_authority& producer_authority, const std::string& url, uint16_t location ) {
+      require_auth( producer );
+
+      check( url.size() < 512, "url too long" );
+      check (checkPermission(producer, "regprod") == 1, "You are not authorised to register as producer");  // LIBRE Check Permissions
+
+      std::visit( [&](auto&& auth ) {
+         check( auth.is_valid(), "invalid producer authority" );
+      }, producer_authority );
+
+      register_producer( producer, producer_authority, url, location );
    }
 
    void system_contract::unregprod( const name& producer ) {
@@ -75,6 +87,137 @@ namespace libresystem {
       const auto& prod = _producers.get( producer.value, "producer not found" );
       _producers.modify( prod, same_payer, [&]( producer_info& info ){
          info.deactivate();
+      });
+   }
+
+   void system_contract::update_elected_producers( const block_timestamp& block_time ) {
+      _gstate.last_producer_schedule_update = block_time;
+
+      auto idx = _producers.get_index<"prototalvote"_n>();
+
+      using value_type = std::pair<eosio::producer_authority, uint16_t>;
+      std::vector< value_type > top_producers;
+      top_producers.reserve(21);
+
+      for( auto it = idx.cbegin(); it != idx.cend() && top_producers.size() < 21 && 0 < it->total_votes && it->active(); ++it ) {
+         top_producers.emplace_back(
+            eosio::producer_authority{
+               .producer_name = it->owner,
+               .authority     = it->get_producer_authority()
+            },
+            it->location
+         );
+      }
+
+      if( top_producers.size() == 0 || top_producers.size() < _gstate.last_producer_schedule_size ) {
+         return;
+      }
+
+      std::sort( top_producers.begin(), top_producers.end(), []( const value_type& lhs, const value_type& rhs ) {
+         return lhs.first.producer_name < rhs.first.producer_name; // sort by producer name
+         // return lhs.second < rhs.second; // sort by location
+      } );
+
+      std::vector<eosio::producer_authority> producers;
+
+      producers.reserve(top_producers.size());
+      for( auto& item : top_producers )
+         producers.push_back( std::move(item.first) );
+
+      if( set_proposed_producers( producers ) >= 0 ) {
+         _gstate.last_producer_schedule_size = static_cast<decltype(_gstate.last_producer_schedule_size)>( top_producers.size() );
+      }
+   }
+
+   double stake2vote( int64_t staked ) {
+      /// TODO subtract 2080 brings the large numbers closer to this decade
+      double weight = int64_t( (current_time_point().sec_since_epoch() - (block_timestamp::block_timestamp_epoch / 1000)) / (seconds_per_day * 7) )  / double( 52 );
+      return double(staked) * std::pow( 2, weight );
+   }
+
+   void system_contract::voteproducer( const name& voter_name, const std::vector<name>& producers ) {
+      require_auth( voter_name );
+      // vote_stake_updater( voter_name );
+      update_votes( voter_name, producers, true );
+      // auto rex_itr = _rexbalance.find( voter_name.value );
+      // if( rex_itr != _rexbalance.end() && rex_itr->rex_balance.amount > 0 ) {
+      //    check_voting_requirement( voter_name, "voter holding REX tokens must vote for at least 21 producers" );
+      // }
+   }
+
+   void system_contract::update_votes( const name& voter_name, const std::vector<name>& producers, bool voting ) {
+      //validate input
+      
+      check( producers.size() <= 30, "attempt to vote for too many producers" );
+      for( size_t i = 1; i < producers.size(); ++i ) {
+         check( producers[i-1] < producers[i], "producer votes must be unique and sorted" );
+      }
+
+      auto voter = _voters.find( voter_name.value );
+      // TODO: UPDATE FOLLOWING VALIDATION
+      check( voter != _voters.end(), "user must stake before they can vote" ); /// staking creates voter object
+
+      /**
+       * The first time someone votes we calculate and set last_vote_weight. Since they cannot unstake until
+       * after the chain has been activated, we can use last_vote_weight to determine that this is
+       * their first vote and should consider their stake activated.
+       */
+      if( _gstate.thresh_activated_stake_time == time_point() && voter->last_vote_weight <= 0.0 ) {
+         _gstate.total_activated_stake += voter->staked;
+         if( _gstate.total_activated_stake >= min_activated_stake ) {
+            _gstate.thresh_activated_stake_time = current_time_point();
+         }
+      }
+
+      auto new_vote_weight = stake2vote( voter->staked );
+
+      std::map<name, std::pair<double, bool /*new*/> > producer_deltas;
+      if ( voter->last_vote_weight > 0 ) {
+         for( const auto& p : voter->producers ) {
+            auto& d = producer_deltas[p];
+            d.first -= voter->last_vote_weight;
+            d.second = false;
+         }
+      }
+
+      if( new_vote_weight >= 0 ) {
+         for( const auto& p : producers ) {
+            auto& d = producer_deltas[p];
+            d.first += new_vote_weight;
+            d.second = true;
+         }
+      }
+
+      const auto ct = current_time_point();
+      double delta_change_rate         = 0.0;
+      double total_inactive_vpay_share = 0.0;
+      for( const auto& pd : producer_deltas ) {
+         auto pitr = _producers.find( pd.first.value );
+         if( pitr != _producers.end() ) {
+            if( voting && !pitr->active() && pd.second.second /* from new set */ ) {
+               check( false, ( "producer " + pitr->owner.to_string() + " is not currently registered" ).data() );
+            }
+            double init_total_votes = pitr->total_votes;
+            _producers.modify( pitr, same_payer, [&]( auto& p ) {
+               p.total_votes += pd.second.first;
+               if ( p.total_votes < 0 ) { // floating point arithmetics can give small negative numbers
+                  p.total_votes = 0;
+               }
+               _gstate.total_producer_vote_weight += pd.second.first;
+               //check( p.total_votes >= 0, "something bad happened" );
+            });
+         } else {
+            if( pd.second.second ) {
+               check( false, ( "producer " + pd.first.to_string() + " is not registered" ).data() );
+            }
+         }
+      }
+
+      // update_total_votepay_share( ct, -total_inactive_vpay_share, delta_change_rate );
+
+      _voters.modify( voter, same_payer, [&]( auto& av ) {
+         av.last_vote_weight = new_vote_weight;
+         av.producers = producers;
       });
    }
 
